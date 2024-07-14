8208aa6f3a9d305cc7af3e2affee403f28d82af3
tlerr <mourahermesonmm9@gmail.com>
2024-07-13 21:54:17 -0300

    attt

--- projetofelas/js/javazinhos.js
+++ projetofelas/js/javazinhos.js
@@ -6,18 +6,18 @@ document.getElementById("imgcontatos").onclick = function() {
 document.getElementById("imghome").onclick = function()
 {
   var estilo = document.getElementById('estilopa');
-  estilo.setAttribute('href', '../Meus css/css pág-contatos.css');
+  estilo.setAttribute('href', '../Meus css/css pág inicial.css');
 };
 
 
 document.getElementById("imgprojetos").onclick = function()
 {
   var estilo = document.getElementById('estilopa');
-  estilo.setAttribute('href', '../Meus css/css pág-contatos.css');
+  estilo.setAttribute('href', '../Meus css/css pág projetos.css');
 };
 
 document.getElementById("imghabilidades").onclick = function()
 {
   var estilo = document.getElementById('estilopa');
-  estilo.setAttribute('href', '../Meus css/css pág-contatos.css');
+  estilo.setAttribute('href', '../Meus css/css pág habilidades.css');
 };
